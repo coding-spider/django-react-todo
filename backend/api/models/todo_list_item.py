a6882dca2fe0e173518e8145ea8deae7f53d587f
rohan <rohan@retailio.in>
2018-06-16 08:54:54 +0000

    added status column in todolistitem

--- backend/api/models/todo_list_item.py
+++ backend/api/models/todo_list_item.py
@@ -3,8 +3,15 @@ from . import BaseModel, TodoList
 
 class TodoListItem(BaseModel):
     """docstring for TodoListItem"""
+
+    STATUSES = (
+        ('PENDING', 'PENDING'),
+        ('COMPLETED', 'COMPLETED')
+    )
+
     todoList = models.ForeignKey(TodoList, on_delete = models.PROTECT, db_column = 'fk_id_todo_list')
     description = models.CharField(max_length = 255)
+    status = models.CharField(max_length = 255, default = 'PENDING', choices = STATUSES)
 
     class Meta:
         db_table = 'todo_list_item'
